ffd869dd46b4d522a687a4ed109881bf290e0213
DotNetPractice <parthivhirani2903@gmail.com>
2023-04-20 14:49:58 +0530

    Added Practical-02 (print bank customer details using printInfo() method

---
+++ Practical02/Customer_Account.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practical02
+{
+    // defining class Customer_Account
+    public class Customer_Account
+    {
+        // parameterized constructor of Customer_Account class
+        public Customer_Account(long customer_accountNo, string customer_name)
+        {
+            this.customer_accountNo = customer_accountNo;
+            this.customer_name = customer_name;
+        }
+
+
+        // variable definition for Customer_Account class
+        // access modifier will be default: internal
+        string bank_name = "SBI";
+        long customer_accountNo;
+        string customer_name;
+
+
+        // printInfo() method will take object's properties and print customer object details in the console
+        internal void printInfo()
+        {
+            Console.WriteLine($"CUSTOMER DETAILS\n------------------\n" +
+                $"Bank Name: {bank_name}\n" +
+                $"Account No.: {customer_accountNo}\n" +
+                $"Customer Name: {customer_name}\n\n");
+        }
+    }
+}
---
+++ Practical02/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practical02
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // instance of Customer_Account class using parameterized constructor
+            Customer_Account customer1 = new Customer_Account(101, "ABC");
+            Customer_Account customer2 = new Customer_Account(102, "XYZ");
+            Customer_Account customer3 = new Customer_Account(204, "MNO");
+
+
+            // calling printInfo() method to print customer details
+            customer1.printInfo();
+            customer2.printInfo();
+        }
+    }
+}

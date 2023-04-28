ef31c57358abd884269ea159b022892bafbec445
DotNetPractice <parthivhirani2903@gmail.com>
2023-04-28 16:46:23 +0530

    Practical - 06 added (Demo of Event in Csharp)

---
+++ Practical06/ProcessBusinessLogic.cs
@@ -0,0 +1,29 @@
+﻿public delegate void Notify();
+class ProcessBusinessLogic
+{
+    public event Notify ProcessCompleted;
+
+    public void StartProcess()
+    {
+        Console.WriteLine("Process Started!");
+        Console.WriteLine("Process is running......");
+        OnProcessCompleted();
+    }
+
+    protected virtual void OnProcessCompleted()
+    {
+        ProcessCompleted?.Invoke();
+    }
+
+    public static void bl_ProcessCompleted()
+    {
+        Console.WriteLine("Method Invoked!");
+    }
+
+    static void Main(string[] args)
+    {
+        ProcessBusinessLogic pbl = new ProcessBusinessLogic();
+        pbl.ProcessCompleted += bl_ProcessCompleted;
+        pbl.StartProcess();
+    }
+}

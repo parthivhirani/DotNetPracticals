8895090cea69ccaa3589bb73e7bad64ce658a6f2
DotNetPractice <parthivhirani2903@gmail.com>
2023-05-11 12:26:10 +0530

    Modified Practical-6 (added comments)

--- Practical06/ProcessBusinessLogic.cs
+++ Practical06/ProcessBusinessLogic.cs
@@ -1,8 +1,13 @@
-﻿public delegate void Notify();
+﻿// defining delegate Notify()
+public delegate void Notify();
+
+// ProcessBusinessLogic class
 class ProcessBusinessLogic
 {
+    // defining ProcessCompleted event
     public event Notify ProcessCompleted;
 
+    // start process method which call OnProcessCompleted() method
     public void StartProcess()
     {
         Console.WriteLine("Process Started!");
@@ -10,16 +15,19 @@ class ProcessBusinessLogic
         OnProcessCompleted();
     }
 
+    // OnProcessCompleted() method which invokes ProcessCompleted event
     protected virtual void OnProcessCompleted()
     {
         ProcessCompleted?.Invoke();
     }
 
+    // bl_ProcessCompleted() method which called when method invoked
     public static void bl_ProcessCompleted()
     {
         Console.WriteLine("Method Invoked!");
     }
 
+    // main method which creates instance of ProcessBusinessLogic class and attach event with it
     static void Main(string[] args)
     {
         ProcessBusinessLogic pbl = new ProcessBusinessLogic();

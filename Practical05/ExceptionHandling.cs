457ddaaae4a45ae40edddf471d0fc68de1e69e27
DotNetPractice <parthivhirani2903@gmail.com>
2023-05-11 12:15:19 +0530

    Modified Practical-5 (added comments)

--- Practical05/ExceptionHandling.cs
+++ Practical05/ExceptionHandling.cs
@@ -1,9 +1,11 @@
 ﻿class ExceptionHandling
 {
+    // creating array of 5 integer elements
     public static int[] myArr = new int[5];
 
     static void Main(string[] args)
     {
+        // taking 5 integer from user and storing it into array
         Console.WriteLine("--Enter 5 array elements--");
         for(int i = 0; i < 5; i++)
         {
@@ -14,6 +16,7 @@
         Console.WriteLine("-------------------");
         try
         {
+            // iterating loop multiple times to generate exception
             for (int i = 0; i < 7; i++)
             {
                 Console.WriteLine($"{i} : {myArr[i]}");
@@ -21,10 +24,12 @@
         }
         catch (Exception ex)
         {
+            // catch block will be executed when exception occurs and prints appropriate message
             Console.WriteLine(ex.ToString());
         }
         finally
         {
+            // finally block will executed regardless of the occurrence of exception
             Console.WriteLine("This is finally block.");
         } 
     }
